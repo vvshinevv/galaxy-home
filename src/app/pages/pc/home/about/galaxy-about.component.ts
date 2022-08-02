d0c18ca0077a61dc8b178a076a50c6cd01daa0ee
DrAnthonyAhn <adh@lingercorp.com>
2022-08-02 16:59:36 +0900

    2022.08.02 팝업은 그대로 내린 상태지만 콘텐츠 코딩, CSS손봤음

--- src/app/pages/pc/home/about/galaxy-about.component.ts
+++ src/app/pages/pc/home/about/galaxy-about.component.ts
@@ -44,7 +44,7 @@ export class GalaxyAboutComponent implements OnInit {
     this.fragmentshaderScript();
     this.init();
     this.animate();
-    this.popupElement.nativeElement.style.display = "none"; //check
+    this.popupElement.nativeElement.style.display = "none";  // 체크!
   }
 
   public vertexshaderScript() {
