e3d2f39110547d7144e18369ba5184f2d91d6303
ahndoro <ahndoro@AhnDongHyun.localdomain>
2022-11-02 18:30:29 +0900

    2022.11.02 신주발행 공고 세팅

--- src/app/pages/pc/home/about/galaxy-about.component.ts
+++ src/app/pages/pc/home/about/galaxy-about.component.ts
@@ -9,7 +9,7 @@ import * as THREE from "three";
 
 @Component({
   selector: "app-galaxy-about",
-  templateUrl: "./galaxy-about.component_notice.html",
+  templateUrl: "./galaxy-about.component_notice_221102.html",
   //templateUrl: "./galaxy-about.component.html",
   styleUrls: ["./galaxy-about.component.css"],
 })
@@ -39,7 +39,7 @@ export class GalaxyAboutComponent implements OnInit {
 
   stats: any;
 
-  constructor(private renderer2: Renderer2) {}
+  constructor(private renderer2: Renderer2) { }
 
   ngOnInit() {
     this.vertexshaderScript();
@@ -47,7 +47,7 @@ export class GalaxyAboutComponent implements OnInit {
     this.init();
     this.animate();
     // Notice On/Off
-    this.popupElement.nativeElement.style.display = "none"; // 체크!
+    // this.popupElement.nativeElement.style.display = "none"; // 체크!
   }
 
   public vertexshaderScript() {
